54be77388bacc6ed7bae542f949612ab92bbaec0
Goston <bcbod2002@gmail.com>
2017-03-21 11:15:03 +0800

    Initial Commit

---
+++ AudioCast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioCast
+//
+//  Created by Goston on 21/03/2017.
+//  Copyright © 2017 Goston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioCast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioCast
+//
+//  Created by Goston on 21/03/2017.
+//  Copyright © 2017 Goston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
