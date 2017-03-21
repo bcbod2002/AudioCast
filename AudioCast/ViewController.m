996d181da97b775ce2ae538d7751af8438823bdd
Goston <bcbod2002@gmail.com>
2017-03-21 17:27:42 +0800

    Change : Implement play songs from iPod library.

--- AudioCast/ViewController.m
+++ AudioCast/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
+#import <MediaPlayer/MPMediaQuery.h>
+#import <MediaPlayer/MPMediaLibrary.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    AVAudioPlayer *player;
+}
 
 @end
 
@@ -17,6 +23,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [MPMediaLibrary requestAuthorization:^(MPMediaLibraryAuthorizationStatus status) {
+        switch (status) {
+            case MPMediaLibraryAuthorizationStatusDenied:
+                NSLog(@"Deny");
+                break;
+            case MPMediaLibraryAuthorizationStatusRestricted:
+                NSLog(@"Restricted");
+                break;
+            case MPMediaLibraryAuthorizationStatusAuthorized:
+                NSLog(@"Authorized");
+                break;
+            case MPMediaLibraryAuthorizationStatusNotDetermined:
+                NSLog(@"Not Determine");
+                break;
+                
+            default:
+                break;
+        }
+    }];
+    
+    MPMediaQuery *mediaQuery = [[MPMediaQuery alloc] init];
+    NSArray<MPMediaItem *> *songsArray = [mediaQuery items];
+    
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    MPMediaItem *mediaItem = [songsArray objectAtIndex:0];
+    NSLog(@"mediaItem = %@", [mediaItem valueForProperty:MPMediaItemPropertyAssetURL]);
+    NSError *error;
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:[mediaItem assetURL] error:&error];
+
+    [player play];
 }
 
 
