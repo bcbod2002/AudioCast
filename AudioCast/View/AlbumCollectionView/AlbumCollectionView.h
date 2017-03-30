441a988aa7f5602073d4dbc84940079b74976538
Goston <bcbod2002@gmail.com>
2017-03-31 00:33:38 +0800

    Add : AlbumCollectionView will list all of the medias which in the library of iPod.

---
+++ AudioCast/View/AlbumCollectionView/AlbumCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionView.h
+//  AudioCast
+//
+//  Created by Goston on 21/03/2017.
+//  Copyright © 2017 Goston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumCollectionView : UICollectionView
+
+@end
---
+++ AudioCast/View/AlbumCollectionView/AlbumCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  AlbumCollectionView.m
+//  AudioCast
+//
+//  Created by Goston on 21/03/2017.
+//  Copyright © 2017 Goston. All rights reserved.
+//
+
+#import "AlbumCollectionView.h"
+
+@implementation AlbumCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
