998a079bb2c7207dfb073a4a259af72ea2b39779
Goston <bcbod2002@gmail.com>
2017-04-02 00:03:37 +0800

    Change : Almost solve the audio delay problem which use the time interval to solve it.

--- AudioCast/ViewController/ConnectionsViewController.m
+++ AudioCast/ViewController/ConnectionsViewController.m
@@ -12,11 +12,13 @@
 #import "GWSimplePlayer.h"
 #import "MPMediaModel.h"
 
-@interface ConnectionsViewController ()
+@interface ConnectionsViewController () <NSURLConnectionDataDelegate>
 
 @property (strong, nonatomic) NSMutableArray<MCPeerID *> *arrConnectedDevices;
 @property (strong, nonatomic) MPMediaModel *mediaModel;
 @property (strong, nonatomic) NSData *mediaData;
+@property (strong, nonatomic) NSURLConnection *connection;
+@property (strong, nonatomic) NSMutableData *mp3Data;
 @property (strong, nonatomic) GWSimplePlayer *simplePlayer;
 
 @end
@@ -31,7 +33,19 @@
     [_manager setDelegate:self];
     
     _arrConnectedDevices = [[NSMutableArray alloc] init];
-    [self initialMPMdeia];
+//    [self initialMPMdeia];
+    
+    // Load Mp3 from url
+    _mp3Data = [[NSMutableData alloc] init];
+    NSURL *mp3URL = [NSURL URLWithString:@"http://picosong.com/cdn/a2b022865d185c4a8508a4212865413f.mp3"];
+//    _connection = [[NSURLConnection alloc] initWithRequest:[NSURLRequest requestWithURL:mp3URL] delegate:self];
+//    _mediaData = [NSData dataWithContentsOfURL:mp3URL];
+    
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSString *myFile = [mainBundle pathForResource:@"Rythem" ofType:@"mp3"];
+    NSLog(@"myFile = %@", myFile);
+    _mediaData = [NSData dataWithContentsOfFile:myFile];
+    
 }
 
 -(void)viewDidAppear:(BOOL)animated {
@@ -65,14 +79,34 @@
 }
 
 - (IBAction)sendAction:(id)sender {
-    NSInteger integer = rand();
-    NSString *string = [NSString stringWithFormat:@"%ld", (long)integer];
-//    [_manager sendString:string];
-
+    NSLog(@"_media lenfht = %ld", [_mediaData length]);
+    NSUInteger dataLength = [_mediaData length];
+    NSMutableArray *dataArray = [[NSMutableArray alloc] init];
+    NSUInteger splitedSize = 10 * 1024;
+    NSUInteger dataCount = (dataLength / splitedSize) + 1;
+    for (NSUInteger i = 0; i < dataCount; ++i) {
+        
+        NSUInteger offset = i * splitedSize;
+        NSUInteger dataSize = dataLength - offset > splitedSize ? splitedSize : dataLength - offset;
+        
+        NSData *splitData = [NSData dataWithBytesNoCopy:(uint8_t *)[_mediaData bytes] + (i * splitedSize) length:dataSize freeWhenDone:NO];
+        [dataArray addObject:splitData];
+    }
     
-    [_manager sendMediaData:_mediaData];
-    [_receiveTextView setText:[NSString stringWithFormat:@"%ld", [_mediaData length]]];
-//    [_receiveTextView setText:string];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        for (NSData *splitedData in dataArray) {
+            [_manager sendMediaData:splitedData];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                static dispatch_once_t onceToken;
+                dispatch_once(&onceToken, ^{
+                    usleep([_manager averageTimeStamp] * 1000);
+                });
+                [_simplePlayer playAudioWithData:splitedData];
+            });
+        }
+    });
+    
+    [_receiveTextView setText:[NSString stringWithFormat:@"%ld", [_mp3Data length]]];
 }
 
 #pragma mark - MCManagerModelDelegate
@@ -91,17 +125,18 @@
 }
 
 -(void)manager:(MCManagerModel *)manager didReceiveData:(NSData *)data fromPeer:(NSString *)peerName {
-//    NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//    NSLog(@"string = %@", string);
-//    [_receiveTextView setText:string];
-    
-    [_simplePlayer play];
     [_simplePlayer playAudioWithData:data];
     [_receiveTextView setText:[NSString stringWithFormat:@"%ld", [data length]]];
 }
 
 -(void)manager:(MCManagerModel *)manager didReceiveStream:(NSInputStream *)stream fromPeer:(NSString *)peerName {
     
+    uint8_t buffer[1024];
+    NSInteger bufferLength = [stream read:buffer maxLength:1024];
+    if (bufferLength > 0) {
+        NSData *data = [NSData dataWithBytes:(const void *)buffer length:bufferLength];
+        [_simplePlayer playAudioWithData:data];
+    }
 }
 
 
@@ -133,6 +168,22 @@
     MCPeerID *peerID = [_arrConnectedDevices objectAtIndex:[indexPath row]];
     
     [browser invitePeer:peerID toSession:session withContext:nil timeout:10];
+}
+
+
+#pragma mark - NSURLConnectionDataDelegate
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+        if ([(NSHTTPURLResponse *) response statusCode] != 200) {
+            NSLog(@"Http code = %ld", [(NSHTTPURLResponse *) response statusCode]);
+            [connection cancel];
+        }
+    }
+}
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    printf("Mp333 data = %ld\n", [data length]);
+    [_mp3Data appendData:data];
 }
 /*
 #pragma mark - Navigation
