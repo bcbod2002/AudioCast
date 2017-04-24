707fb0aca98a812f38e89b734d9b37886110e831
Goston <bcbod2002@gmail.com>
2017-04-24 21:21:37 +0800

    Change : Implement the BluetoothViewController.

--- AudioCast/ViewController/BluetoothViewController.h
+++ AudioCast/ViewController/BluetoothViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+#import "GWBluetoothCentral.h"
+#import "GWBluetoothPeripheral.h"
+
 @interface BluetoothViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *modeLabel;
+@property (weak, nonatomic) IBOutlet UITextView *receiveTextView;
 
 @end
--- AudioCast/ViewController/BluetoothViewController.m
+++ AudioCast/ViewController/BluetoothViewController.m
@@ -8,7 +8,15 @@
 
 #import "BluetoothViewController.h"
 
-@interface BluetoothViewController ()
+#import "GWSimplePlayer.h"
+
+@interface BluetoothViewController () <GWBluetoothCentralDelegate, GWBluetoothPeripheralDelegate> {
+    GWBluetoothCentral *central;
+    GWBluetoothPeripheral *peripheral;
+    
+    GWSimplePlayer *simplePlayer;
+    NSData *mediaData;
+}
 
 @end
 
@@ -17,6 +25,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    
+    [self loadMedia];
+    
+    simplePlayer = [[GWSimplePlayer alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,6 +38,80 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(void)loadMedia {
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    NSString *myFile = [mainBundle pathForResource:@"Rythem" ofType:@"mp3"];
+    mediaData = [NSData dataWithContentsOfFile:myFile];
+}
+
+
+#pragma mark - IBOutlet Action
+- (IBAction)centralButtonAction:(id)sender {
+    central = [[GWBluetoothCentral alloc] initWithDelegate:self];
+    [_modeLabel setText:@"Central Mode"];
+}
+
+- (IBAction)peripheralButtonAction:(id)sender {
+    peripheral = [[GWBluetoothPeripheral alloc] initWithDelegate:self];
+    [_modeLabel setText:@"Peropheral Mode"];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        sleep(5);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"Start advertisement Data");
+            [peripheral startAdvertisementData];
+        });
+    });
+}
+
+- (IBAction)senAudioButtonAction:(id)sender {
+    NSLog(@"_media lenfht = %ld", [mediaData length]);
+    NSUInteger dataLength = [mediaData length];
+    NSMutableArray *dataArray = [[NSMutableArray alloc] init];
+    NSUInteger splitedSize = 10 * 1024;
+    NSUInteger dataCount = (dataLength / splitedSize) + 1;
+    for (NSUInteger i = 0; i < dataCount; ++i) {
+        
+        NSUInteger offset = i * splitedSize;
+        NSUInteger dataSize = dataLength - offset > splitedSize ? splitedSize : dataLength - offset;
+        
+        NSData *splitData = [NSData dataWithBytesNoCopy:(uint8_t *)[mediaData bytes] + (i * splitedSize) length:dataSize freeWhenDone:NO];
+        [dataArray addObject:splitData];
+    }
+    
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        for (NSData *splitedData in dataArray) {
+//            [_mediaData sendMediaData:splitedData];
+//            [peripheral sendMediaData:splitedData];
+            [peripheral sendMediaData:[@"QWERTTT" dataUsingEncoding:NSUTF8StringEncoding]];
+            dispatch_async(dispatch_get_main_queue(), ^{
+//                static dispatch_once_t onceToken;
+//                dispatch_once(&onceToken, ^{
+////                    usleep([mediaData averageTimeStamp] * 1000);
+//                });
+//                [simplePlayer playAudioWithData:splitedData];
+            });
+        }
+//    });
+}
+
+#pragma mark - GWBluetoothCentralDelegate
+-(void)didConnectDevice:(NSString *)deviceName {
+    [_modeLabel setText:[NSString stringWithFormat:@"Central mode : connected %@", deviceName]];
+    NSLog(@"Central Did connected to %@", deviceName);
+}
+
+-(void)didReceiveData:(NSData *)data FromDevice:(NSString *)deviceName {
+    [simplePlayer playAudioWithData:data];
+    [_receiveTextView setText:[NSString stringWithFormat:@"%@-----%ld", deviceName, [data length]]];
+    NSLog(@"Central did receive data from = %@", deviceName);
+    
+}
+
+#pragma mark - GWBluetoothPeripheralDelegate
+-(void)didUpdateState {
+    NSLog(@"Peropheral did update state");
+}
 /*
 #pragma mark - Navigation
 
